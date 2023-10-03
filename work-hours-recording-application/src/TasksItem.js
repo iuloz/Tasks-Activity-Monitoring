2fb60157759f3491399bcc7884c05800aa284a9e
Iurii Lozhkin <lozhkinyulo@gmail.com>
2023-10-03 23:17:03 +0300

    Deleting task components without affecting API

--- work-hours-recording-application/src/TasksItem.js
+++ work-hours-recording-application/src/TasksItem.js
@@ -8,6 +8,7 @@ function TasksItem({ date, task, tag, start, end, hours }) {
     const [taskEditing, setTaskEditing] = useState(false);
     const [tagName, setTagName] = useState(tag);
     const [tagEditing, setTagEditing] = useState(false);
+    const [remove, setRemove] = useState(false);
 
 
     const changeStatus = () => {
@@ -35,43 +36,50 @@ function TasksItem({ date, task, tag, start, end, hours }) {
     }
 
     const deleteTaskComponent = () => {
-
+        setRemove(true);
     }
 
 
 
 
-    return (
-        <div className="task-item" style={{backgroundColor: color}}>
-            <p style={{display:"inline"}}>{date}</p><span onClick={changeStatus} className="status">{status}</span>
-            <p class="task">
-                {taskEditing ?
-                    (<input type="text"
-                            value={taskName}
-                            onChange={e => setTaskName(e.target.value)}
-                            onBlur={renameTask}
-                            autoFocus
-                            style={{height:"10px"}}
-                    />)
-                    :
-                    (<p onDoubleClick={editTask}>{taskName}</p>)}
+    return remove ? null : (
+        <div className="task-item" style={{ backgroundColor: color }}>
+            <p style={{ display: "inline" }}>{date}</p><span onClick={changeStatus} className="status">{status}</span>
+
+            {taskEditing ? (
+                <input
+                    type="text"
+                    value={taskName}
+                    onChange={e => setTaskName(e.target.value)}
+                    onBlur={renameTask}
+                    autoFocus
+                    style={{ height: "10px" }}
+                />
+            ) : (
+                <p className="task" onDoubleClick={editTask}>{taskName}</p>
+            )}
+
+            <p style={{ display: "inline-block" }}>Tag:
+                {tagEditing ? (
+                    <input
+                        type="text"
+                        value={tagName}
+                        onChange={e => setTagName(e.target.value)}
+                        onBlur={renameTag}
+                        autoFocus
+                        style={{ height: "10px", width: "130px" }}
+                    />
+                ) : (
+                    <span className="tag-span" onDoubleClick={editTag}> {tagName}</span>
+                )}
             </p>
-            <p style={{display:"inline-block"}}>Tag: <span class="tag-span">
-                {tagEditing ?
-                    (<input type="text"
-                            value={tagName}
-                            onChange={e => setTagName(e.target.value)}
-                            onBlur={renameTag}
-                            autoFocus
-                            style={{height:"10px", width:"130px"}}
-                    />)
-                    :
-                    (<span class="tag-span" onDoubleClick={editTag}>{tagName}</span>)}
-            </span></p>
+
             <p>Starts: {start}</p>
             <p>Ends: {end}</p>
             <p>Hours: {hours}<span id="delete_task" onClick={deleteTaskComponent}>⤬</span></p>
         </div>
     );
 }
+
+
 export default TasksItem;

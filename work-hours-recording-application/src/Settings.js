c2d990aa617184d465cb02fc7636783134d1bb27
Iurii Lozhkin <lozhkinyulo@gmail.com>
2023-10-18 14:59:01 +0300

    Color theme improved

--- work-hours-recording-application/src/Settings.js
+++ work-hours-recording-application/src/Settings.js
@@ -1,19 +1,33 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function Settings() {
-    const [theme, setTheme] = useState('light');
-    const [mode, setMode] = useState('multiple');
+    const [theme, setTheme] = useState('');
+    const [mode, setMode] = useState('');
 
-    useEffect(() => {
-        const body = document.getElementsByTagName('body')[0];
-            body.style.backgroundColor = (theme === 'light') ? 'whitesmoke' : 'darkgray';
-    }, [theme, mode]);
+
+    const addToApi = async (key, value) => {
+        const requestBody = { [key]: value };
+        await fetch('/settings', {
+            method: 'PATCH',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(requestBody)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                console.log(`${key} changed to '${value}'`);
+            });
+    }
+
 
     const applySettings = () => {
         const newTheme = document.getElementById('theme').value;
         const newMode = document.getElementById('mode').value;
         if (theme !== newTheme) {
             setTheme(newTheme);
+            const body = document.getElementsByTagName('body')[0];
+            body.style.backgroundColor = (newTheme === 'light') ? 'whitesmoke' : '#354657';
+            body.style.color = (newTheme === 'light') ? 'black' : 'whitesmoke';
+            addToApi('theme', newTheme);
         }
         if (mode !== newMode) {
             setMode(newMode);
@@ -24,7 +38,8 @@ function Settings() {
     return (
         <div className='settings'>
             <label htmlFor='theme' style={{ marginRight: '20px' }}>Color theme: </label>
-            <select id='theme' name='theme'>
+            <select id='theme' name='theme' style={{ marginRight: '30px' }} defaultValue={'default'}>
+                <option disabled value='default'>Select Theme</option>
                 <option id='light' value='light'>Light</option>
                 <option id='dark' value='dark'>Dark</option>
             </select>

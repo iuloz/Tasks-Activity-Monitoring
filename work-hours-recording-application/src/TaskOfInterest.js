d57737d2b8e81a045563e0f9f9c53d66124dfe9c
Iurii Lozhkin <lozhkinyulo@gmail.com>
2023-10-30 16:52:07 +0200

    Taks details interval error handling

--- work-hours-recording-application/src/TaskOfInterest.js
+++ work-hours-recording-application/src/TaskOfInterest.js
@@ -10,26 +10,34 @@ function TaskOfInterest(props) {
     const [activityPeriods, setActivityPeriods] = useState([]);
     const [addingPeriod, setAddingPeriod] = useState(false);
     const [newPeriod, setNewPeriod] = useState('');
-    const [taskIntervals, setTaskIntervals] = useState([]); // [{start: ..., end: ...}]
+    // const [taskIntervals, setTaskIntervals] = useState([]); // [{start: ..., end: ...}]
     const [startTimes, setStartTimes] = useState([]); // ['','','','']
     const [endTimes, setEndTimes] = useState([]); // ['','','','']
+    const [startEditing, setStartEditing] = useState(null);
+    const [endEditing, setEndEditing] = useState(null);
 
 
     useEffect(() => {
         let starts = [];
         let ends = [];
-        let intervals = activityPeriods.map((interval, index) => {
+        activityPeriods.forEach((interval, index) => {
             if (interval.task === props.task) {
                 starts.push(interval.start);
                 ends.push(interval.end);
-                return {
-                    start: interval.start,
-                    end: interval.end
-                }
             }
-            return null;
         });
-        setTaskIntervals(intervals);
+        // let intervals = activityPeriods.map((interval, index) => {
+        //     if (interval.task === props.task) {
+        //         starts.push(interval.start);
+        //         ends.push(interval.end);
+        //         return {
+        //             start: interval.start,
+        //             end: interval.end
+        //         }
+        //     }
+        //     return null;
+        // });
+        // setTaskIntervals(intervals);
         setStartTimes(starts);
         setEndTimes(ends);
     }, [activityPeriods, props.task]);
@@ -102,9 +110,6 @@ function TaskOfInterest(props) {
     }
 
     const deletePeriod = index => {
-        // let updatedPeriods = [...activityPeriods];
-        // updatedPeriods.splice(index, 1);
-        // setActivityPeriods(updatedPeriods);
         let updatedStarts = [...startTimes];
         let updatedEnds = [...endTimes];
         updatedStarts.splice(index, 1);
@@ -114,52 +119,124 @@ function TaskOfInterest(props) {
     }
 
     const addPeriod = () => {
-        const [periodStart, periodEnd] = newPeriod.split(' - ');
-        let tempStarts = [...startTimes, periodStart];
-        console.log(tempStarts);
-        let tempEnds = [...endTimes, periodEnd];
-        console.log(tempEnds);
-        setNewPeriod('');
+        const regex = /^\d{2}\.\d{2}\.\d{4}, \d{2}:\d{2}:\d{2} - \d{2}\.\d{4}, \d{2}:\d{2}:\d{2}$/;
+        if (regex.test(newPeriod)) {
+            const [periodStart, periodEnd] = newPeriod.split(' - ');
+            let tempStarts = [...startTimes, periodStart];
+            console.log(tempStarts);
+            let tempEnds = [...endTimes, periodEnd];
+            console.log(tempEnds);
+            setNewPeriod('');
 
-        const activityStartDates = tempStarts.map(date => {
-            const periodStartDate = new Date(date.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
-            return periodStartDate;
-        });
-
-        const activityEndDates = tempEnds.map(date => {
-            const periodEndDate = new Date(date.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
-            return periodEndDate;
-        });
-
-        const sortedStartDates = activityStartDates.sort((a, b) => a - b);
-        const sortedEndDates = activityEndDates.sort((a, b) => a - b);
-        const sortedStarts = sortedStartDates.map(start => {
-            const startString = start.toLocaleString('ru-RU', {
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
+            const activityStartDates = tempStarts.map(date => {
+                const periodStartDate = new Date(date.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+                return periodStartDate;
             });
-            return startString;
-        });
 
-        const sortedEnds = sortedEndDates.map(end => {
-            const endString = end.toLocaleString('ru-RU', {
-                day: '2-digit',
-                month: '2-digit',
-                year: 'numeric',
-                hour: '2-digit',
-                minute: '2-digit',
-                second: '2-digit',
+            const activityEndDates = tempEnds.map(date => {
+                const periodEndDate = new Date(date.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+                return periodEndDate;
             });
-            return endString;
-        });
-        setStartTimes(sortedStarts);
-        setEndTimes(sortedEnds);
-        setAddingPeriod(false);
+
+            const sortedStartDates = activityStartDates.sort((a, b) => a - b);
+            const sortedEndDates = activityEndDates.sort((a, b) => a - b);
+            const sortedStarts = sortedStartDates.map(start => {
+                const startString = start.toLocaleString('ru-RU', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                });
+                return startString;
+            });
+
+            const sortedEnds = sortedEndDates.map(end => {
+                const endString = end.toLocaleString('ru-RU', {
+                    day: '2-digit',
+                    month: '2-digit',
+                    year: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    second: '2-digit',
+                });
+                return endString;
+            });
+            setStartTimes(sortedStarts);
+            setEndTimes(sortedEnds);
+            setAddingPeriod(false);
+        } else {
+            alert('Please type interval in correct format');
+        }
     }
+
+    const handleStartEdit = (index, editedStart) => {
+        const updatedStarts = [...startTimes];
+        updatedStarts[index] = editedStart;
+        setStartTimes(updatedStarts);
+    }
+
+    const editStart = (index, editedStart) => {
+        if (editedStart.trim() !== '') {
+            const updatedStarts = [...startTimes];
+            const editedStartDate = new Date(editedStart.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+            const endDate = new Date(endTimes[index].replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+            console.log(endTimes[index - 1]);
+            if (endTimes[index - 1]) {
+                const prevEndDate = new Date(endTimes[index - 1].replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+                if (editedStartDate >= prevEndDate && editedStartDate <= endDate) {
+                    updatedStarts[index] = editedStart;
+                    setStartTimes(updatedStarts);
+                    setStartEditing(null);
+                } else {
+                    alert('Date must be after previous stop time and before this stop time');
+                }
+            } else {
+                if (editedStartDate <= endDate) {
+                    updatedStarts[index] = editedStart;
+                    setStartTimes(updatedStarts);
+                    setStartEditing(null);
+                } else {
+                    alert('Date must be before stop time');
+                }
+            }
+        }
+    }
+
+    const handleEndEdit = (index, editedEnd) => {
+        const updatedEnds = [...endTimes];
+        updatedEnds[index] = editedEnd;
+        setEndTimes(updatedEnds);
+    }
+
+    const editEnd = (index, editedEnd) => {
+        if (editedEnd.trim() !== '') {
+            const updatedEnds = [...endTimes];
+            const editedEndDate = new Date(editedEnd.replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+            const startDate = new Date(startTimes[index].replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+            if (startTimes[index + 1]) {
+                const nextStartDate = new Date(startTimes[index + 1].replace(/(\d{2}).(\d{2}).(\d{4}), (\d{2}):(\d{2}):(\d{2})/, "$3-$2-$1T$4:$5:$6"));
+                if (editedEndDate >= startDate && editedEndDate <= nextStartDate) {
+                    updatedEnds[index] = editedEnd;
+                    setEndTimes(updatedEnds);
+                    setEndEditing(null);
+                } else {
+                    alert('Date must be after start time and before next start time');
+                }
+            } else {
+                if (editedEndDate >= startDate) {
+                    updatedEnds[index] = editedEnd;
+                    setEndTimes(updatedEnds);
+                    setEndEditing(null);
+                } else {
+                    alert('Date must be after start time');
+                }
+            }
+
+        }
+    }
+
 
 
 
@@ -191,9 +268,34 @@ function TaskOfInterest(props) {
                             startTimes.map((time, index) => {
                                 return (
                                     <div key={index}>
-                                        <p style={{ display: 'inline-block', cursor: 'pointer' }}>{time}</p>
+                                        {
+                                            (startEditing !== index) ? (
+                                                <p onClick={() => setStartEditing(index)} style={{ display: 'inline-block', cursor: 'pointer' }}>{time}</p>
+                                            ) : (
+                                                <input
+                                                    type='text'
+                                                    value={time}
+                                                    onChange={e => handleStartEdit(index, e.target.value)}
+                                                    onBlur={e => editStart(index, e.target.value)}
+                                                    autoFocus
+                                                />
+                                            )
+                                        }
                                         <span> - </span>
-                                        <p style={{ display: 'inline-block', cursor: 'pointer' }}>{endTimes[index]}</p>
+                                        {
+                                            (endEditing !== index) ? (
+                                                <p onClick={() => setEndEditing(index)} style={{ display: 'inline-block', cursor: 'pointer' }}>{endTimes[index]}</p>
+                                            ) : (
+                                                <input
+                                                    type='text'
+                                                    value={endTimes[index]}
+                                                    onChange={e => handleEndEdit(index, e.target.value)}
+                                                    onBlur={e => editEnd(index, e.target.value)}
+                                                    autoFocus
+                                                />
+                                            )
+                                        }
+
                                         <span onClick={() => deletePeriod(index)} style={{ color: 'red', cursor: 'pointer', marginLeft: '15px' }}>⤬</span>
                                     </div>
                                 );
